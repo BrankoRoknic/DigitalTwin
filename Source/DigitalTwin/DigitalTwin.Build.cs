b66ca2d5f4f3374967fec71534fbfeec5f6b2bb2
Branko Roknic <102108647@student.swin.edu.au>
2024-08-07 20:33:23 +1000

    Rebuild UE project from scratch

--- Source/DigitalTwin/DigitalTwin.Build.cs
+++ Source/DigitalTwin/DigitalTwin.Build.cs
@@ -1,6 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using System.IO;
 using UnrealBuildTool;
 
 public class DigitalTwin : ModuleRules
@@ -8,29 +7,14 @@ public class DigitalTwin : ModuleRules
 	public DigitalTwin(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PublicDependencyModuleNames.AddRange(new[]
-		{
-			"Core", "CoreUObject", "Engine", "InputCore", "HTTP", "UMG", "Json",
-			"JsonUtilities", "AWSSDK", "AWSPlugin"
-		});
-		bEnableUndefinedIdentifierWarnings = false;
-
-		PrivateDependencyModuleNames.AddRange(new[]
-		{
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"HTTP", // Include Http module
-			"Json",
-			"JsonUtilities"
-		});
-		
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
+		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
--- Source/DigitalTwinEditor.Target.cs
+++ Source/DigitalTwinEditor.Target.cs
@@ -1,10 +1,11 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.Collections.Generic;
 
 public class DigitalTwinEditorTarget : TargetRules
 {
-	public DigitalTwinEditorTarget(TargetInfo Target) : base(Target)
+	public DigitalTwinEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
